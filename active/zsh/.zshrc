6cc94b2c5aeb8a9cc7b975eeacc5077d0c0eac24
Brennan Meadowcroft <brennan@Brennans-MacBook-Pro.local>
2016-02-08 21:18:08 -0700

    Update software installs and tweak configs

--- active/zsh/.zshrc
+++ active/zsh/.zshrc
@@ -92,3 +92,7 @@ export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 
 export NVM_DIR="/Users/A777967/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+source ~/.bash_profile
+source ~/.bashrc
+
+
--- setup/run_config.sh
+++ setup/run_config.sh
@@ -6,7 +6,7 @@ rm ~/.gitignore_global && n -s .git/.gitignore_global ~/.gitignore_global
 echo "Adding dotfiles"
 rm ~/.bash_profile && ln -s $(pwd)/active/bash/.bash_profile ~/.bash_profile
 rm ~/.bashrc && ln -s $(pwd)/active/bash/.bashrc ~/.bashrc
-rm ~/.gitconfig && ln -s $(pwd)/active/git/.gitconfig ~/.gitconfig
+# rm ~/.gitconfig && cp $(pwd)/active/git/.gitconfig ~/.gitconfig
 echo -e "\e[31mImportant! Don't forget to add email to .gitconfig\e[0m"
 rm ~/.zshrc && ln -s $(pwd)/active/zsh/.zshrc ~/.zshrc
 
