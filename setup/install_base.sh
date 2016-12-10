1146b98f49be1b75d8521221306bde6f68be90b5
Brennan Meadowcroft <brennan.meadowcroft@gmail.com>
2016-12-10 11:39:33 -0700

    Tmux tweaks

--- setup/install_base.sh
+++ setup/install_base.sh
@@ -1,47 +1,50 @@
 #! /bin/bash
+echo $pwd
+echo "Initializing submodules"
+#git submodule init && git submodule update
 
-if eval "brew -v"; then
-  echo "Homebrew detected"
-  brew update
-else
-  echo "Homebrew not detected... installing now"
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+#if eval "brew -v"; then
+#  echo "Homebrew detected"
+#  brew update
+#else
+#  echo "Homebrew not detected... installing now"
+#  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#fi
 
-echo "Bootstrapping computer setup.  This could take a while..."
-echo "Installing basic components"
-# Install RVM
-if eval "rvm -v"; then
-  echo "RVM installed"
-else
-  curl -sSL https://get.rvm.io | bash -s stable --ruby
-fi
+#echo "Bootstrapping computer setup.  This could take a while..."
+#echo "Installing basic components"
+## Install RVM
+#if eval "rvm -v"; then
+#  echo "RVM installed"
+#else
+#  curl -sSL https://get.rvm.io | bash -s stable --ruby
+#fi
 
-# Install NVM and node
-if eval "nvm --version"; then
-  echo "NVM installed"
-else
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
-  nvm install stable
-  nvm alias default stable
-fi
+## Install NVM and node
+#if eval "nvm --version"; then
+#  echo "NVM installed"
+#else
+#  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+#  nvm install stable
+#  nvm alias default stable
+#fi
 
-# Install base software
-cat $(pwd)/software-list/homebrew-software.txt | xargs brew install
-cat $(pwd)/software-list/gems-software.txt | xargs gem install
-cat $(pwd)/software-list/npm-global-software.txt | xargs npm install -g
-cat $(pwd)/software-list/brew-cask-software.txt | xargs brew cask install
+## Install base software
+#cat $(pwd)/software-list/homebrew-software.txt | xargs brew install
+#cat $(pwd)/software-list/gems-software.txt | xargs gem install
+#cat $(pwd)/software-list/npm-global-software.txt | xargs npm install -g
+#cat $(pwd)/software-list/brew-cask-software.txt | xargs brew cask install
 
-echo "Installing OSX Xcode Command Line Tools"
-if eval "xcode-select -v"; then
-  echo "Xcode tools already installed"
-else
-  echo "Xcode Tools not detected... installing now"
-  xcode-select --install
-fi
+#echo "Installing OSX Xcode Command Line Tools"
+#if eval "xcode-select -v"; then
+#  echo "Xcode tools already installed"
+#else
+#  echo "Xcode Tools not detected... installing now"
+#  xcode-select --install
+#fi
 
-#source setup/install_brew_cask.sh
-source setup/run_config.sh
+##source setup/install_brew_cask.sh
+#source setup/run_config.sh
 
-#echo "Preparing for projects"
-# mkdir ~/projects
+##echo "Preparing for projects"
+## mkdir ~/projects
